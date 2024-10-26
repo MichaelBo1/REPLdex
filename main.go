bf890d53acfb3cdd209e04cebd1b5021c3d9578c
MichaelBo1 <92109316+MichaelBo1@users.noreply.github.com>
2024-10-26 14:52:39 +0100

    Basic REPL loop with commands

--- main.go
+++ main.go
@@ -6,17 +6,62 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("Pokedex > ")
-		ok := scanner.Scan()
+		scanner.Scan()
+
+		cmdName := scanner.Text()
+
+		cmd, ok := getCommands()[cmdName]
 		if !ok {
-			break
+			fmt.Println("unknown command")
+			continue
 		}
 
-		cmd := scanner.Text()
-		fmt.Println(cmd)
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
