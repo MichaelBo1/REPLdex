4ca75e1cf659a921a8e8b867b80c8547a60d228d
MichaelBo1 <92109316+MichaelBo1@users.noreply.github.com>
2024-10-26 15:00:45 +0100

    Dir cleanup

---
+++ repl.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		cmdName := scanner.Text()
+
+		cmd, ok := getCommands()[cmdName]
+		if !ok {
+			fmt.Println("unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
